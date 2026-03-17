08ca348faf530baaec73361d42e3be5c3f220132
anglingkusumandhita <anglingkusumandhita@gmail.com>
2026-03-17 21:12:05 +0700

    fix specified key wa to long

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Support older MySQL / MariaDB installations that still have 1000-byte index limits.
+        Schema::defaultStringLength(191);
+
         RateLimiter::for('contact-messages', function (Request $request): array {
             $email = Str::lower((string) $request->input('email'));
 
